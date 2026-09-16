5b4d6426c3e0794758aa43b93e65dcd982de1dac
Mohammed Suhaib <raksssuhaib@gmail.com>
2026-09-17 02:53:21 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sn-mehendi-v1';
+const assetsToCache = [
+  'index.html',
+  'manifest.json',
+  'logo.jpg',
+  'logo-512.png',
+  'logo-192.png',
+  'background.jpg'
+];
+
+// Install Service Worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(assetsToCache);
+      })
+  );
+});
+
+// Fetch assets from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
